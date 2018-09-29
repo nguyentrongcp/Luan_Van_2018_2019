2be21fbd898042dfc5c6a1d4c0e64058da7212f4
Nguyễn Trọng <nguyentrongcp@gmail.com>
2018-09-29 23:26:07 +0700

    home-page-building-nav-mobile

--- Project/resources/views/customer/layouts/partials/footer.blade.php
+++ Project/resources/views/customer/layouts/partials/footer.blade.php
@@ -1,3 +1,24 @@
+<footer>
+    <div class="row blue-grey darken-4">
+        <div class="container">
+            <div class="col s12 m12 l4" style="padding: 20px 10px 20px 0">
+                <iframe class="z-depth-1" src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FFastfoodyvn-
+            Th%25E1%25BB%25A9c-%25C4%2583n-nhanh-ngon-ti%25E1%25BB%2587n-l%25E1%25BB%25A3i-260893111234023%2F%3Fmodal%3
+            Dadmin_todo_tour&tabs&width=340&height=196&small_header=false&adapt_container_width=true&hide_cover=false&s
+            how_facepile=true&appId"
+                        width="340" height="200" style="border:none;overflow:hidden" scrolling="no" frameborder="0"
+                        allowTransparency="true" allow="encrypted-media"></iframe>
+            </div>
+            <div class="col s12 m12 l4" style="padding: 20px 10px 20px 0">
+                <iframe class="z-depth-1" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d207443.7928317165!2d139.601
+                29408485906!3d35.66938631031498!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188b857628235d
+                %3A0xcdd8aef709a2b520!2sTokyo%2C+Japan!5e0!3m2!1sen!2s!4v1538237936793" width="400" height="200"
+                        frameborder="0" style="border:0" allowfullscreen></iframe>
+            </div>
+        </div>
+    </div>
+</footer>
+
     <script src="customer/js/jquery-3.3.1.min.js"></script>
 
     <!--JavaScript at end of body for optimized loading-->
--- Project/resources/views/customer/layouts/partials/navbar.blade.php
+++ Project/resources/views/customer/layouts/partials/navbar.blade.php
@@ -5,14 +5,14 @@
                 <a href="#" class="brand-logo left">
                     <img class="responsive-img" src="customer/image/logo.png">
                 </a>
-                <ul id="nav-mobile" class="right hide-on-med-and-down">
+                <ul class="right hide-on-med-and-down">
                     <li><a href="#"><b>Tạo tài khoản</b></a></li>
                     <li><a href="#"><b>Đăng nhập</b></a></li>
                     <li><a href="#">
                             <i class="material-icons">shopping_cart</i>
                         </a></li>
                 </ul>
-                <a href="#" data-target="slide-out" class="navbar-fixed sidenav-trigger hide-on-large-only right-align">
+                <a href="#" data-target="nav-mobile" class="navbar-fixed sidenav-trigger hide-on-large-only right-align">
                     <i class="material-icons" style="font-size: 30px">menu</i></a>
             </div>
         </div>
@@ -20,21 +20,114 @@
 
 </div>
 
-<ul id="slide-out" class="sidenav">
-    <li><div class="user-view">
-            <div class="background">
-                <img src="images/office.jpg">
-            </div>
-            <a href="#user"><img class="circle" src="images/yuna.jpg"></a>
-            <a href="#name"><span class="white-text name">John Doe</span></a>
-            <a href="#email"><span class="white-text email">jdandturk@gmail.com</span></a>
-        </div></li>
-    <li><a href="#!"><i class="material-icons">cloud</i>First Link With Icon</a></li>
-    <li><a href="#!">Second Link</a></li>
-    <li><div class="divider"></div></li>
-    <li><a class="subheader">Subheader</a></li>
-    <li><a class="waves-effect" href="#!">Third Link With Waves</a></li>
+<ul id="nav-mobile" class="sidenav">
+    {{--<li><div class="user-view">--}}
+            {{--<div class="background">--}}
+                {{--<img src="images/office.jpg">--}}
+            {{--</div>--}}
+            {{--<a href="#user"><img class="circle" src="images/yuna.jpg"></a>--}}
+            {{--<a href="#name"><span class="white-text name">John Doe</span></a>--}}
+            {{--<a href="#email"><span class="white-text email">jdandturk@gmail.com</span></a>--}}
+        {{--</div></li>--}}
+    {{--<li><a href="#!"><i class="material-icons">cloud</i>First Link With Icon</a></li>--}}
+    {{--<li><a href="#!">Second Link</a></li>--}}
+    {{--<li><div class="divider"></div></li>--}}
+    {{--<li><a class="subheader">Subheader</a></li>--}}
+    {{--<li><a class="waves-effect" href="#!">Third Link With Waves</a></li>--}}
+    <li>
+        <a class="waves-effect" href="#">
+            Tạo tài khoản
+        </a>
+    </li>
+    <li>
+        <a class="waves-effect" href="#">
+            Đăng nhập
+        </a>
+    </li>
+    <li class="search">
+        <div class="search-wrapper">
+            <input id="search-mobile" placeholder="Tìm kiếm...">
+            <div class="search-results"></div>
+        </div>
+    </li>
+    {{--<li><div class="divider"></div></li>--}}
+    {{--<li>--}}
+        {{--<a class="waves-effect" href="#">--}}
+            {{--<i class="material-icons left">shopping_cart</i>--}}
+            {{--Giỏ hàng--}}
+        {{--</a>--}}
+    {{--</li>--}}
+    {{--<li><div class="divider"></div></li>--}}
+    <li>
+        <ul class="collapsible">
+            <li>
+                <div class="collapsible-header waves-effect waves-purple navbar-mobile">Thức ăn</div>
+                <div class="collapsible-body">
+                    <ul>
+                        <li>
+                            <a href="#" class="waves-effect">First</a>
+                        </li>
+                        <li>
+                            <a href="#" class="waves-effect">Second</a>
+                        </li>
+                        <li>
+                            <a href="#" class="waves-effect">Third</a>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+            <li>
+                <div class="collapsible-header waves-effect navbar-mobile">Nước uống</div>
+                <div class="collapsible-body">
+                    <ul>
+                        <li>
+                            <a href="#" class="waves-effect">First</a>
+                        </li>
+                        <li>
+                            <a href="#" class="waves-effect">Second</a>
+                        </li>
+                        <li>
+                            <a href="#" class="waves-effect">Third</a>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+        </ul>
+    </li>
+    <li class="bold">
+        <a class="waves-effect" href="#">
+            Tạo tài khoản
+        </a>
+    </li>
+    <li>
+        <a class="waves-effect" href="#">
+            Đăng nhập
+        </a>
+    </li>
 </ul>
+
+<style>
+    .navbar-mobile {
+        padding: 0 32px !important;
+        font-weight: 500;
+    }
+    .collapsible-body .waves-effect {
+        padding: 0 47px !important;
+    }
+    #nav-mobile li.search .search-wrapper {
+        color: #777;
+        margin-top: -1px;
+        border-top: 1px solid rgba(0,0,0,0.14);
+        -webkit-transition: margin .25s ease;
+        transition: margin .25s ease;
+    }
+    #nav-mobile .search .search-wrapper input {
+        border-bottom: 1px solid rgba(0,0,0,0.14);
+    }
+    #search-mobile {
+        padding: 0 32px;
+    }
+</style>
 
 @push('script')
     <script>
@@ -42,6 +135,7 @@
             $('.sidenav').sidenav({
                 edge: 'right'
             });
+            $('.collapsible').collapsible();
         });
     </script>
 @endpush
