419e6b9f36958fb3015d576b01866b978ac95d03
dobyboy <lytruonggiang2510@gmail.com>
2018-10-26 20:15:46 +0700

    search foodies

--- Project/resources/views/admin/layouts/partials/scripting.blade.php
+++ Project/resources/views/admin/layouts/partials/scripting.blade.php
@@ -26,29 +26,13 @@
     $("#gallery-avatar-image").change(function() {
         readURL(this);
     });
+    $.ajaxSetup({
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
+    });
 </script>
 <script>
-    var categoryContent = [
-        { category: 'South America', title: 'Brazil' },
-        { category: 'South America', title: 'Peru' },
-        { category: 'North America', title: 'Canada' },
-        { category: 'Asia', title: 'South Korea' },
-        { category: 'Asia', title: 'Japan' },
-        { category: 'Asia', title: 'China' },
-        { category: 'Europe', title: 'Denmark' },
-        { category: 'Europe', title: 'England' },
-        { category: 'Europe', title: 'France' },
-        { category: 'Europe', title: 'Germany' },
-        { category: 'Africa', title: 'Ethiopia' },
-        { category: 'Africa', title: 'Nigeria' },
-        { category: 'Africa', title: 'Zimbabwe' },
-    ];
-    $('.ui.search')
-        .search({
-            type: 'category',
-            source: categoryContent
-        })
-    ;
     $(document).ready(
         function () {
             $('.ui.checkbox').checkbox();
@@ -70,91 +54,3 @@
     CKEDITOR.replace('ckeditor', options);
 </script>
 
-{{--<script>--}}
-    {{--// script đặc biệt cần sử dụng blade--}}
-    {{--function toggleSidebar() {--}}
-        {{--let wide = '{{ $wideMenu }}';--}}
-        {{--let collapsed = wide != '1';--}}
-        {{--let sidebarWide = $('#sidebar-wide');--}}
-        {{--let sidebarThin = $('#sidebar-thin');--}}
-        {{--let mainContainer = $('#main-container');--}}
-        {{--let logo = $('#logo');--}}
-        {{--return function () {--}}
-            {{--if (collapsed) {--}}
-                {{--loadWideSidebar(sidebarWide, sidebarThin, mainContainer, logo);--}}
-                {{--axios.get('/admin/menu_state/1');--}}
-                {{--collapsed = false;--}}
-                {{--return;--}}
-            {{--}--}}
-            {{--loadThinSidebar(sidebarWide, sidebarThin, mainContainer, logo);--}}
-            {{--axios.get('/admin/menu_state/0');--}}
-            {{--collapsed = true;--}}
-        {{--};--}}
-    {{--}--}}
-
-    {{--function loadWideSidebar(sidebarWide, sidebarThin, mainContainer, logo) {--}}
-        {{--let width = 220;--}}
-        {{--let margin = 220;--}}
-        {{--let duration = 350;--}}
-        {{--$(logo).animate({width: width}, duration);--}}
-        {{--$(mainContainer).animate({marginLeft: margin}, duration);--}}
-        {{--$('#btn-toggle-menu').find('i').removeClass('right').addClass('left');--}}
-        {{--$(sidebarWide).transition('slide right', duration);--}}
-        {{--$(sidebarThin).transition('slide right', duration);--}}
-    {{--}--}}
-
-    {{--function loadThinSidebar(sidebarWide, sidebarThin, mainContainer, logo) {--}}
-        {{--let width = 50;--}}
-        {{--let margin = 50;--}}
-        {{--let duration = 350;--}}
-        {{--$(logo).animate({width: width}, duration);--}}
-        {{--$(mainContainer).animate({marginLeft: margin}, duration);--}}
-        {{--$('#btn-toggle-menu').find('i').removeClass('left').addClass('right');--}}
-        {{--$(sidebarWide).transition('slide right', duration);--}}
-        {{--$(sidebarThin).transition('slide right', duration);--}}
-    {{--}--}}
-
-    {{--$('#btn-toggle-menu').click(toggleSidebar());--}}
-
-    {{--// (function(e) {--}}
-    {{--//     let timeout;--}}
-    {{--//     window.addEventListener('resize', function(e) {--}}
-    {{--//         clearTimeout(timeout);--}}
-    {{--//         timeout = setTimeout(function() {--}}
-    {{--//             console.log(e);--}}
-    {{--//         }, 300);--}}
-    {{--//     });--}}
-    {{--// })();--}}
-
-    {{--function buildChart(id, type, dataSource, name, customColors = null) {--}}
-        {{--let ctx = document.getElementById(id).getContext('2d');--}}
-        {{--let colors = customColors || ['#36A2EB', '#4BC0C0', '#FFCD56', '#FF9F40', '#FF6384','#f57f17','#1565c0', '#004d40', '#827717'];--}}
-        {{--// let colors = customColors || ['#56E289', '#E2CF56', '#E256AE', '#56AEE2', '#E28956','#f57f17','#1565c0', '#004d40', '#827717'];--}}
-        {{--let dataVals = [], labels = [], bgColors = [];--}}
-
-        {{--dataSource.forEach(function(datum, idx) {--}}
-            {{--bgColors.push(colors[idx]);--}}
-            {{--dataVals.push(datum.total);--}}
-            {{--labels.push(datum[name]);--}}
-        {{--});--}}
-        {{--let data = {--}}
-                {{--datasets: [{--}}
-                    {{--data: dataVals,--}}
-                    {{--backgroundColor: bgColors--}}
-                {{--}],--}}
-                {{--labels: labels--}}
-            {{--};--}}
-        {{--new Chart(ctx, {--}}
-            {{--type: type,--}}
-            {{--data: data,--}}
-            {{--options: {--}}
-                {{--pieceLabel: {--}}
-                    {{--render: 'percentage',--}}
-                    {{--frontColor: ['black'],--}}
-                    {{--precision: 2--}}
-                {{--}--}}
-            {{--}--}}
-        {{--});--}}
-    {{--}--}}
-
-{{--</script>--}}
---
+++ Project/resources/views/admin/statistic/revenue/table.blade.php
@@ -0,0 +1,54 @@
+<table class="ui very compact table celled striped center aligned" id="revenue-table">
+    <thead>
+    <tr><th id="duration"></th><th>Mua vào</th><th>Bán ra</th><th>Hiệu số</th></tr>
+    </thead>
+    <tbody>
+
+    </tbody>
+</table>
+
+@push('script')
+    <script>
+        function renderTable(source) {
+            let revenues = source.data.revenues;
+            let buyings = source.data.buyings;
+            $('#duration').text(source.axes.x);
+            let tbody = buildTableBody(revenues, buyings);
+            $('#account-table').find('tbody').html(tbody);
+        }
+
+        function buildTableBody(revenues, buyings) {
+            let tbody = '';
+            let revenueSum = 0, buyingSum = 0, roi = 0;
+            for (let i = 0; i < revenues.length; i++) {
+                tbody += buildRow(revenues[i], buyings[i]);
+                revenueSum += parseFloat(revenues[i].value);
+                buyingSum += parseFloat(buyings[i].value);
+                roi += (revenues[i].value - buyings[i].value);
+            }
+
+            tbody += `<tr>
+                <td><strong>Tổng cộng:</strong></td>
+                <td><strong>${buyingSum.toFixed(2)}</strong></td>
+                <td><strong>${revenueSum.toFixed(2)}</strong></td>
+                <td><strong>${roi.toFixed(2)}</strong></td>
+            </tr>`;
+
+            rows.push(['Tong cong', buyingSum.toFixed(2), revenueSum.toFixed(2), roi.toFixed(2)]);
+            return tbody;
+        }
+
+        function buildRow(revenue, buying) {
+            if (revenue.value == 0 && buying.value == 0)
+                return '';
+            let tr = '';
+            tr += `<tr>
+                <td><strong>${revenue.label}</strong></td>
+                <td>${buying.value}</td>
+                <td>${revenue.value}</td>
+                <td>${(revenue.value - buying.value).toFixed(2)}</td>
+            </tr>`;
+            return tr;
+        }
+    </script>
+@endpush
