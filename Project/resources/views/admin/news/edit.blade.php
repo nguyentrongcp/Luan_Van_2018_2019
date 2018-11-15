19b799d81efd6269e17e0baf02c1556e66cf46eb
dobyboy <lytruonggiang2510@gmail.com>
2018-11-15 16:28:07 +0700

    ...

--- Project/resources/views/admin/news/edit.blade.php
+++ Project/resources/views/admin/news/edit.blade.php
@@ -12,15 +12,40 @@
 
         @include('admin.layouts.components.success_msg')
 
-        <form action="{{ route('news.update', [$news->id]) }}" method="post" class="ui form">
+        <form action="{{ route('news.update', [$news->id]) }}" method="post" class="ui form" enctype="multipart/form-data">
             {{ method_field('PUT') }}
             {{ csrf_field() }}
 
             <div class="ui stackable grid">
-                <div class="eleven wide column">
+                <div class="ten wide column">
                     <div class="field">
                         <label>Tiêu đề</label>
                         <input type="text"  name="title" placeholder="Tiêu đề" value="{{ $news->title }}" required>
+                    </div>
+                </div>
+                @if($errors->has('title'))
+                    <div style="color: red; margin-top: 5px; font-size: 13px">
+                        {{ $errors->first('title') }}
+                    </div>
+                @endif
+                <div class="six wide column">
+                    <div class="required field">
+                        <label>Ảnh đại diện</label>
+                        <img src="{{asset($news->avatar)}}" class="ui img-tb" alt="{{$news->title}}">
+                        <label for="avatar-news">
+                            <span class="ui blue compact label">Chọn một ảnh</span>
+                            <span id="avatar-news-name"></span>
+                        </label>
+                        <input type="file" name="avatar-news" id="avatar-news" style="display: none;"
+                               onchange="$('#avatar-news-name').text($('#avatar-news')[0].files[0].name)"
+                               accept=".jpg, .png, .jpeg">
+                    </div>
+                    @if($errors->has('avatar-news'))
+                        <div style="color: red; margin-top: 5px; font-size: 13px">
+                            {{ $errors->first('avatar-news') }}
+                        </div>
+                    @endif
+                    <div class="ui divider">
                     </div>
                 </div>
             </div>
